0842a727f63f19e10a5592ddb4583d738c1f8829
viktor <162162844+vikxzzzz@users.noreply.github.com>
2024-11-01 16:28:08 -0300

    Update code.js

--- DOM/code.js
+++ DOM/code.js
@@ -1,46 +1,41 @@
-    
 const url = "https://botafogo-atletas.mange.li/2024-1/"
 
-    
 const pega_json = async (caminho) => {
-        const resposta = await fetch(caminho); 
-        const dados = await resposta.json();
-        return dados 
-    }
-    
+    const resposta = await fetch(caminho);
+    const dados = await resposta.json();
+    return dados;
+}
+
 const container = document.getElementById("container");
-    
+
 const montaCard = (atleta) => {
-    const cartao = document.createElement("article"); 
+    const cartao = document.createElement("article");
     const nome = document.createElement("h1");
     const imagem = document.createElement("img");
-    const descri = document.createElement("p"); 
+    const descri = document.createElement("p");
 
     nome.innerHTML = atleta.nome;
     nome.style.fontFamily = "sans-serif"
-    cartao.appendChild(nome); 
+    cartao.appendChild(nome);
 
-    imagem.src = atleta.imagem; 
+    imagem.src = atleta.imagem;
     cartao.appendChild(imagem)
 
-    descri.innerHTML = atleta.detalhes; 
-    cartao.appendChild(descri); 
+    descri.innerHTML = atleta.detalhes;
+    cartao.appendChild(descri);
 
     return cartao
 }
-    
 
-pega_json(`${url}feminino`).then(
+
+pega_json(`${url}masculino`).then(
     (r) => {
-        r.array.forEach(
+        r.forEach(
             (ele) => container.appendChild(montaCard(ele))
-            
-        );
+
+        )
     }
-    
-
-
-    )
+);
 
 
 
