058e130d78e36cee143794052e3f8d1e8d3abea2
viktor <162162844+vikxzzzz@users.noreply.github.com>
2024-11-01 16:03:37 -0300

    dom

---
+++ DOM/code.js
@@ -0,0 +1,34 @@
+    const container = document.getElementById("container");
+    
+    const montaCard = (atleta) => {
+        const cartao = document.createElement("article"); 
+        const nome = document.createElement("h1");
+        const imagem = document.createElement("img");
+        const descri = document.createElement("p"); 
+
+        nome.innerHTML = atleta.nome;
+        nome.style.fontFamily = "s"
+        cartao.appendChild(nome); 
+
+        imagem.src = atleta.imagem; 
+        cartao.appendChild(imagem)
+
+        descri.innerHTML = atleta.detalhes; 
+        cartao.appendChild(descri); 
+
+        return cartao
+    }
+        /*container.innerHTML += `
+        <article>
+            <h1>${atleta.nome}</h1>
+            <img src='${atleta.imagem}'>
+            <p>${atleta.detalhes}</p> 
+        </article>    
+        `*/
+
+
+    dados.forEach(
+        (ele) => container.appendChild(montaCard(ele))
+    )
+
+
