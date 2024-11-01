6ae2a738aa1bb588aa7255eb69957ed6ca765908
viktor <162162844+vikxzzzz@users.noreply.github.com>
2024-11-01 16:18:42 -0300

    g

--- DOM/code.js
+++ DOM/code.js
@@ -1,34 +1,57 @@
-    const container = document.getElementById("container");
     
-    const montaCard = (atleta) => {
-        const cartao = document.createElement("article"); 
-        const nome = document.createElement("h1");
-        const imagem = document.createElement("img");
-        const descri = document.createElement("p"); 
+const url = "https://botafogo-atletas.mange.li/2024-1/"
 
-        nome.innerHTML = atleta.nome;
-        nome.style.fontFamily = "sans-serif"
-        cartao.appendChild(nome); 
-
-        imagem.src = atleta.imagem; 
-        cartao.appendChild(imagem)
-
-        descri.innerHTML = atleta.detalhes; 
-        cartao.appendChild(descri); 
-
-        return cartao
+    
+const pega_json = async (caminho) => {
+        const resposta = await fetch(caminho); 
+        const dados = await resposta.json();
+        return dados 
     }
-        /*container.innerHTML += `
+    
+const container = document.getElementById("container");
+    
+const montaCard = (atleta) => {
+    const cartao = document.createElement("article"); 
+    const nome = document.createElement("h1");
+    const imagem = document.createElement("img");
+    const descri = document.createElement("p"); 
+
+    nome.innerHTML = atleta.nome;
+    nome.style.fontFamily = "sans-serif"
+    cartao.appendChild(nome); 
+
+    imagem.src = atleta.imagem; 
+    cartao.appendChild(imagem)
+
+    descri.innerHTML = atleta.detalhes; 
+    cartao.appendChild(descri); 
+
+    return cartao
+}
+    
+
+pega_json(`${url}feminino`).then(
+    (r) => {
+        r.array.forEach(
+            (ele) => container.appendChild(montaCard(ele))
+            
+        );
+    }
+    
+
+
+    )
+
+
+
+/*dados.forEach(
+    (ele) => container.appendChild(montaCard(ele))
+)*/
+
+/*container.innerHTML += `
         <article>
             <h1>${atleta.nome}</h1>
             <img src='${atleta.imagem}'>
             <p>${atleta.detalhes}</p> 
         </article>    
         `*/
-
-
-    dados.forEach(
-        (ele) => container.appendChild(montaCard(ele))
-    )
-
-
